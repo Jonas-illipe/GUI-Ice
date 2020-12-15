14e5642b9aebabb332b873aae6069231601e7b69
Jonas <jonas.illipe@elev.ga.lba.se>
2020-12-15 15:54:48 +0100

    lagt till grejer
    ;D

--- GUI-Ice-Game/Assets/Script/Healthbar.cs
+++ GUI-Ice-Game/Assets/Script/Healthbar.cs
@@ -5,15 +5,16 @@ using UnityEngine.UI;
 
 public class Healthbar : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public Slider slider;
+
+    public void SetMaxHealth (int health)
     {
-        
+        slider.maxValue = health;
+        slider.value = health;
+    }
+    public void SetHealth(int health)
+    {
+        slider.value = health;
     }
 }
